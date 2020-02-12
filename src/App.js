f6064b9abba58ee7d2f2f7883a7b8f9db7d65465
DIAO <isolvethis@gmail.com>
2020-02-12 11:12:35 -0800

    Added Version2 - Which adds a dismiss feature and use list as App's local state

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import './App.css';
 /**
  * Author: DIAO 
  * Date : 2/11/2020
+ * Version : 2 (based on v1)
  * Description : 
- * In this version, the App component will render a list outside of class. 
+ *  Previous version the App component will render a list outside of class. 
+ *  What's new in this version ? 
+ * 
+ *  1. In render()  mapping list within App's list instead of list outside of class 
+ *  2. Add onDismiss to dismiss items on click (also binded the function in constructor)
  */
 
 
@@ -33,22 +38,59 @@ const list = [
 
 
 class App extends React.Component {
+  /**
+   * 
+   * Calling super(props) is required. This enables this.props to be accessibile in constructor
+   */
+  constructor(props) {
+    
+    super(props);
+    /** Setup state  */
+    this.state = {
+        list, /** ES 6 Feature ,   list : list  can be just list if property and variable share same name */
+    };
+
+    /** Bind class member function: onDismiss() to make it a class method so "this" is accessible to onDimiss()  */
+    this.onDismiss = this.onDismiss.bind(this);   
+
+  }//END Constructor 
+
+  /** Returns an updated list without item with objectID === id */
+  /** This function gets invoked when button is hit which after generating updated list then call setState() to render this component */
+  onDismiss(id) {
+    console.log("onDimiss Clicked");
+    const updatedList = this.state.list.filter((item) => item.objectID !== id);  
+    this.setState({list: updatedList});
+  }
+
+  /**
+   * Map each element in list and display its content 
+   */
   render(){
       return(
         <div className="App">
            {
-             list.map((item) =>      
+             //Mapping App's list , NOT the list outside of class ! 
+             this.state.list.map((item) =>      
                  <div key={item.objectID}>
                    <span> <a href={item.url}>{item.title} </a></span>
                    <span>{item.author} </span>
                    <span>{item.num_comments} </span>
                    <span>{item.points} </span>
+                   <span>
+                     <button
+                        onClick={ ()=> this.onDismiss(item.objectID) }
+                        type="button"
+                     >
+                       Dismiss 
+                     </button>
+                   </span>
                   </div>
              )
            }
         </div>
       )
-  }
-}
+  }//END RENDER() 
+}//END CLASS APP
 
 export default App;
