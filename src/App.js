c6c776e8462d4bc82cc9c891cd847fe95e1b9dad
DIAO <isolvethis@gmail.com>
2020-02-11 22:08:47 -0800

    Added list which is array of objects to use for later.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import React from 'react';
-
 import './App.css';
+
+/**
+ * Author: DIAO 
+ * Date : 2/11/2020
+ * Description : 
+ * In this version, the App component will render a list outside of class. 
+ */
+
+
+/**
+ * Array of Objects 
+ */
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 
 function App() {
   return (
