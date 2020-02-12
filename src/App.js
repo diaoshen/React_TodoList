b37fb2643e7c3b0ff2a9915e98a3dae9c23b9048
DIAO <isolvethis@gmail.com>
2020-02-11 22:10:27 -0800

    Changed App from functional to class component

--- src/App.js
+++ src/App.js
@@ -32,12 +32,23 @@ const list = [
 ];
 
 
-function App() {
-  return (
-    <div className="App">
-        HelloWorld!
-    </div>
-  );
+class App extends React.Component {
+  render(){
+      return(
+        <div className="App">
+           {
+             list.map((item) =>      
+                 <div key={item.objectID}>
+                   <span> <a href={item.url}>{item.title} </a></span>
+                   <span>{item.author} </span>
+                   <span>{item.num_comments} </span>
+                   <span>{item.points} </span>
+                  </div>
+             )
+           }
+        </div>
+      )
+  }
 }
 
 export default App;
